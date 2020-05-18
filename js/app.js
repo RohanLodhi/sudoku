e2630394fc81bbb66c0d8c91aba1318e5d4bc4d9
RohanLodhi <rogth7@gmail.com>
2020-05-19 03:14:22 +0530

    declare fix

--- js/app.js
+++ js/app.js
@@ -1,33 +1,34 @@
+'use strict';
 const submit = document.querySelector('button')
 
 const sliceArray = arr => {
-    row1 = arr.slice(0,9);
-    row2 = arr.slice(9,18);
-    row3 = arr.slice(18,27);
-    row4 = arr.slice(27,36);
-    row5 = arr.slice(36,45);
-    row6 = arr.slice(45,54);
-    row7 = arr.slice(54,63);
-    row8 = arr.slice(63,72);
-    row9 = arr.slice(72,81);
+    const row1 = arr.slice(0,9);
+    const row2 = arr.slice(9,18);
+    const row3 = arr.slice(18,27);
+    const row4 = arr.slice(27,36);
+    const row5 = arr.slice(36,45);
+    const row6 = arr.slice(45,54);
+    const row7 = arr.slice(54,63);
+    const row8 = arr.slice(63,72);
+    const row9 = arr.slice(72,81);
     
     arr = [row1,row2,row3,row4,row5,row6,row7,row8,row9];
     return arr;
 };
 
 const checkSudoku = board => {
-    for(i=0;i<9;i++){
+    for(let i=0;i<9;i++){
         let val = [];
-        for(j=0;j<9;j++){
+        for(let j=0;j<9;j++){
             if(val.includes(board[j][i]) && board[j][i] !== 0){
                 return true;
             }
             val.push(board[j][i]);
         }
     }
-    for(i=0;i<9;i++){
+    for(let i=0;i<9;i++){
         let val = []
-        for(j=0;j<9;j++){
+        for(let j=0;j<9;j++){
             if(val.includes(board[i][j]) && board[i][j] !== 0){
                 return true;
             }
@@ -53,6 +54,19 @@ const alertUser = () => {
     });
 }
 
+const clearInputs = () => {
+    const button = document.querySelector('.display');
+    button.style.display = 'block'
+
+    button.addEventListener('click', () =>{
+        const clearInputs = document.querySelectorAll('input')
+        clearInputs.forEach(input => {
+            input.value = ''
+        })
+        button.style.display = 'none'
+    })
+}
+
 submit.addEventListener('click', e => {
     e.preventDefault();
     let values = [];
@@ -65,7 +79,7 @@ submit.addEventListener('click', e => {
         }
     });
     console.log(values)
-    sudoku = sliceArray(values);
+    let sudoku = sliceArray(values);
     if(checkSudoku(sudoku)){
         alertUser()
     }
@@ -74,18 +88,18 @@ submit.addEventListener('click', e => {
         if(!sudoku){
             alertUser()
         } else{
-            i=0;
-            j=0;
+            let a=0;
+            let b=0;
             console.log('trying')
             document.querySelectorAll('input').forEach(input => {
-                input.value = sudoku[j][i];
-                i++;
-                if(i===9){
-                    i = 0;
-                    j++
+                input.value = sudoku[b][a];
+                a++;
+                if(a===9){
+                    a = 0;
+                    b++
                 }
             })
-            console.log(sudoku)
+            clearInputs()
         }
     }
 
--- js/solver.js
+++ js/solver.js
@@ -1,6 +1,7 @@
+'use strict';
 const posCheck = (sudoku, pos) => {
-    for(col=0;col<9;col++){
-        for(row=0;row<9;row++){
+    for(let col=0;col<9;col++){
+        for(let row=0;row<9;row++){
             if(sudoku[row][col] == 0){
                 pos[0] = row;
                 pos[1] = col;
@@ -12,7 +13,7 @@ const posCheck = (sudoku, pos) => {
 }
 
 const usedInCol = (sudoku,col,num) => {
-    for(i=0;i<9;i++){
+    for(let i=0;i<9;i++){
         if(sudoku[i][col] === num){
             return false;
         }
@@ -21,7 +22,7 @@ const usedInCol = (sudoku,col,num) => {
 }
 
 const usedInRow = (sudoku,row,num) => {
-    for(i=0;i<9;i++){
+    for(let i=0;i<9;i++){
         if(sudoku[row][i] === num){
             return false;
         }
@@ -30,11 +31,11 @@ const usedInRow = (sudoku,row,num) => {
 }
 
 const usedInBox = (sudoku, pos, num) => {
-    row = Math.floor(pos[0]/3)*3;
-    col = Math.floor(pos[1]/3)*3;
+    const row = Math.floor(pos[0]/3)*3;
+    const col = Math.floor(pos[1]/3)*3;
 
-    for(i=row;i<row+3;i++){
-        for(j=col;j<col+3;j++){
+    for(let i=row;i<row+3;i++){
+        for(let j=col;j<col+3;j++){
             if(sudoku[i][j] === num){
                 return false;
             }
@@ -47,7 +48,6 @@ const numCheck = (sudoku,pos,i) => {
     if(usedInCol(sudoku,pos[1],i)){
         if(usedInRow(sudoku,pos[0],i)){
             if(usedInBox(sudoku,pos,i)){
-                console.log('possible')
                 return true;
             }
         }
@@ -55,12 +55,12 @@ const numCheck = (sudoku,pos,i) => {
     return false;
 }
 const sudokuSolver = (sudoku) => {
-    pos = [0,0];
-    availablePos = posCheck(sudoku, pos);
+    let pos = [0,0];
+    const availablePos = posCheck(sudoku, pos);
     if(!availablePos){
         return true;
     }
-    for(i=1;i<10;i++){
+    for(let i=1;i<10;i++){
         if(numCheck(sudoku,availablePos,i)){
             sudoku[availablePos[0]][availablePos[1]] = i;
 
